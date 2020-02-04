ad4be0a1ea74a969d3c109f0153349365b8741a8
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-04 09:53:28 -0700

    add price test

--- test/house_test.rb
+++ test/house_test.rb
@@ -9,5 +9,10 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     assert_instance_of House, house
   end
+
+  def test_it_has_price
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal house.price, "$400000"
+  end
   
 end
