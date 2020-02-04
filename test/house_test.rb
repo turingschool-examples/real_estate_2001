211e0c2aefca6dbfad4dc5cfe3a1bc3dac84917d
Chris Stankus <chrisstankus99@gmail.com>
2020-02-04 09:29:02 -0700

    Add the add room test and functionality

--- test/house_test.rb
+++ test/house_test.rb
@@ -13,4 +13,16 @@ class HouseTest < Minitest::Test
   def test_it_exists
     assert_instance_of House, @house
   end
+
+  def test_it_has_attributes
+    assert_equal "$400000", @house.price
+    assert_equal "123 sugar lane", @house.address
+    assert_equal [], @house.rooms
+  end
+
+  def test_it_can_add_room
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    assert_equal [@room_1, @room_2], @house.rooms
+  end
 end
