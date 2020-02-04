48daec2697969418ee5982c1510a2ed617606ca3
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-04 09:49:32 -0700

    add it_exists test

---
+++ test/house_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+
+class HouseTest < Minitest::Test
+
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+    assert_instance_of House, house
+  end
+  
+end
