211fa0b5c2c61dc51d393166b3518bdfe1eaf1be
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-04 10:47:59 -0700

    add total area test

--- test/house_test.rb
+++ test/house_test.rb
@@ -54,4 +54,17 @@ class HouseTest < Minitest::Test
     assert_equal house.rooms_from_category(:basement), [room_4]
   end
 
+  def test_it_can_get_total_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal house.area, 1900
+  end
+
 end
