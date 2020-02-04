bfb04d7ec45c7074ad46d7e2d77f5a5deec5eb12
Mariana Cid <cidmariana5@gmail.com>
2020-02-04 09:06:00 -0700

    Area method complete.

--- test/room_test.rb
+++ test/room_test.rb
@@ -14,7 +14,6 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_can_get_area
-    skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
