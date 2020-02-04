3bd1374e0d45b1fe01fb942f75598b2947653d10
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-04 09:10:43 -0700

    Complete iteration 1 with the addition of paint and is_painted method with passing tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,23 @@
-class Rom
+class Room
+
+  attr_reader :category, :width, :length, :is_painted
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
+    @length = length
     @width = width
+    @is_painted = false
   end
 
   def area
-    @length * @width
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    is_painted
+  end
+
+  def paint
+    @is_painted = true
   end
 end
