cc39243a181e492c7dc32eaea4cd8eb5670ac107
Mariana Cid <cidmariana5@gmail.com>
2020-02-04 09:16:01 -0700

    test file for house created.

---
+++ test/house_test.rb
@@ -0,0 +1,12 @@
+require './lib/room'
+require './lib/house'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class HouseTest < Minitest::Test
+
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+    assert_instance_of House, house
+  end
+end
