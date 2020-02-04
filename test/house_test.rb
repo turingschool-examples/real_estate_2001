6d7b0f7f32c099e8db5f12f43453d82573eeb4c3
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-04 11:03:18 -0700

    Add method to calcualte price per square foot

--- test/house_test.rb
+++ test/house_test.rb
@@ -4,7 +4,7 @@ require './lib/house'
 require './lib/room'
 
 class HouseTest < Minitest::Test
-  
+
   def test_it_exists
     house = House.new("$400000", "123 sugar lane")
 
@@ -84,4 +84,18 @@ class HouseTest < Minitest::Test
     assert_equal house_details, house.details
   end
 
+  def test_price_per_square_foot
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+    assert_equal 210.53, house.price_per_square_foot
+  end
 end
