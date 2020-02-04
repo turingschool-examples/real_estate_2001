a30ebd06a8d5f4276372547b1392485fa7bf9c04
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-04 10:08:50 -0700

    Update house class with add_room method

--- test/house_test.rb
+++ test/house_test.rb
@@ -8,6 +8,8 @@ class HouseTest < Minitest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room1 = Room.new(:bedroom, 10, '13')
+    @room2 = Room.new(:living_room, 15, '12')
   end
 
   def test_it_exists
@@ -24,6 +26,13 @@ class HouseTest < Minitest::Test
 
   def test_it_start_with_no_rooms
     assert_equal [], @house.rooms
+  end
+
+  def test_it_can_add_rooms
+    @house.add_room(@room1)
+    @house.add_room(@room2)
+    assert_equal [@room1, @room2], @house.rooms
+
   end
 
 end
