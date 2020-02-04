9aba22382425a5d41c38555ce2c3dc913d324709
Andrew Reid <areid.sj@gmail.com>
2020-02-04 09:14:22 -0700

    Create house test file and write it exists test

---
+++ test/house_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/test'
+
+class RoomTest < Minitest::Test
+
+  def setup
+    @house = House.new("$400000", "123 sugar lane")
+  end
+
+  def test_it_exists
+    assert_instance_of House, @house
+  end
+
+end
