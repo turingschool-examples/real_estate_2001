ff00122e0e6bcf5682889100a5da29811eba3f4e
“Krista <stadler.krista@gmail..com>
2020-02-04 09:43:35 -0700

    Test is has rooms passes

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,10 @@
 class House
-  attr_reader :price, :address
+  attr_reader :price, :address, :rooms
 
   def initialize(price, address)
     @price = price.gsub(/\D/,'').to_i
     @address = address
+    @rooms = []
   end
 
 end
