b5cf4448a11c96d32c2fdd246c2600026801dea3
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 09:33:24 -0700

    Add get_cost method

--- lib/house.rb
+++ lib/house.rb
@@ -1,8 +1,12 @@
 class House
+  attr_reader :cost, :address
 
   def initialize(cost, address)
     @cost = cost
     @address = address
   end
 
+  def get_cost
+    @cost.gsub("$", '').to_i
+  end
 end
