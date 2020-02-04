43705f3d696f9e794ec238240806277d5a6ba972
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-04 09:11:01 -0700

    Update length instance

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,7 +4,7 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
   def test_it_exists
-    
+
     room = Room.new(:bedroom, 10, '13')
     assert_instance_of Room, room
   end
@@ -16,7 +16,7 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_can_get_area
-    skip
+
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
