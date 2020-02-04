c62a56ce2cec9c38f2ecf54d39a21ce56755200e
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-04 10:07:05 -0700

    Add iteration 3 tests

--- test/house_test.rb
+++ test/house_test.rb
@@ -14,6 +14,12 @@ class HouseTest < Minitest::Test
     assert_equal [], house.rooms
   end
 
+  def test_it_can_return_details
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal {"price" => 400000, "address" => "123 sugar lane"}, house.details
+  end
+
   def test_it_can_have_rooms
     house = House.new("$400000", "123 sugar lane")
     room_1 = Room.new(:bedroom, 10, '13')
@@ -29,9 +35,51 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     room_1 = Room.new(:bedroom, 10, '13')
     room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
     house.add_room(room_1)
     house.add_room(room_2)
 
     assert_equal [room_1, room_2], house.rooms
+
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2, room_3, room_4], house.rooms
+  end
+
+  def test_it_can_return_rooms_from_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_it_can_return_house_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+  end
+
+  def test_it_can_be_above_market_average?
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal false, house.above_market_average?
   end
 end
