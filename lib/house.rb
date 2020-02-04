7e8868e5148f5a4f533baafd26739479c9b7554f
Alex Lynch <mlynch5187@gmail.com>
2020-02-04 09:24:10 -0700

    Complete iteration_2

--- lib/house.rb
+++ lib/house.rb
@@ -1,12 +1,18 @@
 class House
+  attr_reader :address, :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
   end
 
   def price
     @price[1..-1].to_i
+  end
+
+  def add_room(room)
+    @rooms << room
   end
 
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -7,6 +7,8 @@ class HouseTest < MiniTest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
   end
 
   def test_houses_exist
@@ -17,27 +19,21 @@ class HouseTest < MiniTest::Test
     assert_equal 400000, @house.price
   end
 
+  def test_houses_have_addresses
+    assert_equal "123 sugar lane", @house.address
+  end
+
+  def test_houses_have_rooms
+    assert_equal [], @house.rooms
+  end
+
+  def test_rooms_can_be_added_to_houses
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    assert_equal [@room_1, @room_2], @house.rooms
+  end
 end
 
-# pry(main)> house.price
-# #=> 400000
-#
-# pry(main)> house.address
-# #=> "123 sugar lane"
-#
-# pry(main)> house.rooms
-# #=> []
-#
-# pry(main)> room_1 = Room.new(:bedroom, 10, '13')
-# #=> #<Room:0x00007fccd29b5720...>
-#
-# pry(main)> room_2 = Room.new(:bedroom, 11, '15')
-# #=> #<Room:0x00007fccd2985f48...>
-#
-# pry(main)> house.add_room(room_1)
-#
-# pry(main)> house.add_room(room_2)
-#
 # pry(main)> house.rooms
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
 # ```
