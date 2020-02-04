4e94b4f9ac179e6455a952a0009176b0739aae1f
Anthony Gomes <code@tonygom.es>
2020-02-04 09:13:54 -0700

    Add Room class w/ passing tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,22 @@
-class Rom
+class Room
+  attr_reader :category
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
-    @width = width
+    @length = length.to_i
+    @width = width.to_i
+    @status = false
   end
 
   def area
     @length * @width
+  end
+
+  def is_painted?
+    @status
+  end
+
+  def paint
+    @status = true
   end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -3,22 +3,32 @@ require 'minitest/pride'
 require './lib/room'
 
 class RoomTest < Minitest::Test
+  def setup
+    @room1 = Room.new(:bedroom, 10, '13')
+    @room2 = Room.new(:living_room, 15, '12')
+  end
+
   def test_it_exists
-    room = Room.new(:bedroom, 10, '13')
-    assert_instance_of Room, room
+    assert_instance_of Room, @room1
   end
 
   def test_it_has_a_category
-    skip
-    room = Room.new(:bedroom, 10, '13')
-    assert_equal :bedroom, room.category
+    assert_equal :bedroom, @room1.category
   end
 
   def test_it_can_get_area
-    skip
-    room1 = Room.new(:bedroom, 10, '13')
-    room2 = Room.new(:living_room, 15, '12')
-    assert_equal 130, room1.area
-    assert_equal 180, room2.area
+    assert_equal 130, @room1.area
+    assert_equal 180, @room2.area
+  end
+
+  def test_it_starts_painted_and_can_be_painted
+    refute @room1.is_painted?
+    refute @room2.is_painted?
+
+    @room1.paint
+    @room2.paint
+
+    assert @room1.is_painted?
+    assert @room2.is_painted?
   end
 end
