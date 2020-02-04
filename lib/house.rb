38d28408cf9a5b9aa118b00d7d351f76bbae3320
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-04 10:19:35 -0700

    def above_market_average

--- lib/house.rb
+++ lib/house.rb
@@ -12,4 +12,11 @@ class House
     @rooms << room
   end
 
+  def above_market_average?
+    house_price = @price.chars.each do |char|
+      char.to_i
+    end
+    house_price.join.to_i > 500000
+  end
+
 end
