3d2f4af7fe320ce90f93f03b5d21423f8243c1a7
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-04 10:16:33 -0700

    Update above_market? method to remove '$' and add price per_square_foot method, pass all tests

--- lib/house.rb
+++ lib/house.rb
@@ -11,11 +11,18 @@ attr_reader :price, :address, :rooms
   end
 
   def above_market?
-    if @price.to_i > 500000
+    if @price.delete('$').to_i > 500000
       true
     else
       false
     end
   end
 
+  def price_per_square_foot
+    house_total_square_feet = 0
+    @rooms.each do |room|
+      house_total_square_feet += room.area
+    end
+    ((@price.delete('$').to_i / house_total_square_feet.to_f)).round(2)
+  end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -8,6 +8,8 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, '13')
     @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
   end
 
   def test_it_exists
@@ -36,9 +38,21 @@ class HouseTest < Minitest::Test
 
     assert_equal [@room_1, @room_2], @house.rooms
   end
-  
+
   def test_it_is_above_market?
+    house2 = House.new("$800000", "456 hello avenue")
 
     assert_equal false, @house.above_market?
+    assert_equal true, house2.above_market?
   end
+
+  def test_it_can_calculate_house_price_per_square_foot
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    assert_equal 210.53, @house.price_per_square_foot
+  end
+
 end
