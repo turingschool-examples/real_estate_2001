0e6b9198a7161c782efe3bf09fb9efc3411fc7fd
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 09:28:18 -0700

    changed the rooms test to check if rooms have been added to the array

--- test/house_test.rb
+++ test/house_test.rb
@@ -23,5 +23,10 @@ class HouseTest < Minitest::Test
   def test_if_house_has_rooms
     house = House.new("$400000", "123 sugar lane")
     assert_equal [], house.rooms
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    assert_equal [room_1,room_2], house.rooms
   end
 end
