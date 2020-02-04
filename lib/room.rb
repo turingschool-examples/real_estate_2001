267eafde2cca64c80d2944c948a01d7b575eac29
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-04 09:07:28 -0700

    Add attr_reader: category and pass test_it_has_category

--- lib/room.rb
+++ lib/room.rb
@@ -1,10 +1,12 @@
-class Rom
-
+class Room
+attr_reader :category
   def initialize(category, length, width)
     @category = category
     @length = 10
     @width = width
   end
+
+
 
   def area
     @length * @width
