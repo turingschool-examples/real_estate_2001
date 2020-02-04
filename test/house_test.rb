eddf7b82cfca37c7f1a9d23191740ab11636ac73
Andrew Reid <areid.sj@gmail.com>
2020-02-04 09:31:17 -0700

    Write rooms from category test

--- test/house_test.rb
+++ test/house_test.rb
@@ -9,6 +9,8 @@ class RoomTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, '13')
     @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
     # require "pry"; binding.pry
   end
 
@@ -36,6 +38,12 @@ class RoomTest < Minitest::Test
 
   def test_if_it_is_above_market_average
     assert_equal false, @house.above_market_average?
+  end
+
+  def test_it_has_rooms_from_category
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
+    assert_equal [], @house.rooms_from_category(:kitchen)
   end
 
 end
