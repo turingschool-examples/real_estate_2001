d603b09b1d770f49d23686f6a945d5ddd1e1f4b3
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 09:58:58 -0700

    Update logic

--- lib/room.rb
+++ lib/room.rb
@@ -1,11 +1,10 @@
 class Room
-  attr_reader :category, :area
+  attr_reader :category
 
   def initialize(category, length, width)
     @category = category
     @length = length.to_i
     @width = width.to_i
-    @area = nil
     @painted = false
   end
 
