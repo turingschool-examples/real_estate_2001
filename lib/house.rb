109dd39cf6df554ff447014a04105dc641aca52b
Ryan Camp <cmpprg@icloud.com>
2020-02-04 09:34:11 -0700

    Create attr_readers for @attributes and mod price to be int w/out $

--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,10 @@
 class House
-
+  attr_reader :price, :address, :rooms
+  
   def initialize(value, address)
-
+    @price = (value.sub("$", "")).to_i
+    @address = address
+    @rooms = []
   end
+
 end
