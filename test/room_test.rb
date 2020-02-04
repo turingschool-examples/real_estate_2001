a96c265fdc28b5b2199f120142e3338b722fb179
Josh Tukman <josh.tukman@gmail.com>
2020-02-04 09:07:18 -0700

    Add test for category

--- test/room_test.rb
+++ test/room_test.rb
@@ -3,13 +3,13 @@ require 'minitest/pride'
 require './lib/room'
 
 class RoomTest < Minitest::Test
+
   def test_it_exists
     room = Room.new(:bedroom, 10, '13')
     assert_instance_of Room, room
   end
 
   def test_it_has_a_category
-    skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
