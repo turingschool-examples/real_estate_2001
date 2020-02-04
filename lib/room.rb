a93093ec634c0041b074b2261eb9bfd91737d7f0
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-04 09:13:00 -0700

    Edit room file to make existing tests pass. Add basic is_painted? method

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,28 @@
-class Rom
+class Room
+
+  attr_reader :category
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
+    @length = length
     @width = width
   end
 
   def area
-    @length * @width
+    @length * @width.to_i
+  end
+
+  def is_painted?
+
   end
 end
+
+
+#
+# pry(main)> room.is_painted?
+# #=> false
+#
+# pry(main)> room.paint
+#
+# pry(main)> room.is_painted?
+# #=> true
