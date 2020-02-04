31b4e83b3f4c045b36f00943bdd32a3717d104fa
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-04 09:46:28 -0700

    Adds test for above or below market average

--- test/house_test.rb
+++ test/house_test.rb
@@ -29,4 +29,37 @@ class RoomTest < Minitest::Test
     assert_equal [room_1, room_2], house.rooms
   end
 
+  def test_it_can_be_above_or_below_market_average
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal false, house.above_market_average?
+  end
+
+  def test_it_can_return_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2],house.rooms_from_category(:bedroom)
+    assert_equal [room_4]house.rooms_from_category(:basement)
+  end
+
 end
+
+# Use TDD to update your House class so that it responds
+# to the following interaction pattern. A house is
+# above_market_average if its price is greater than
+# $500,000.
+
+
+#house.area
+#=> 1900
+
+#house.details
+#=> {"price" => 400000, "address" => "123 sugar lane"}
