f2d4e8afbca0c83ed2f336c033c2b1e8999c7a37
“Krista <stadler.krista@gmail..com>
2020-02-04 09:36:03 -0700

    Test it has a price written

--- test/house_test.rb
+++ test/house_test.rb
@@ -6,16 +6,19 @@ require './lib/house'
 class HouseTest < Minitest::Test
 
   def test_it_exists
+    skip
     house = House.new("$400000", "123 sugar lane")
     assert_instance_of House, house
   end
 
+  def test_it_has_a_price
+    
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal 40000, house.price
+  end
 
 end
 
-
-#  pry(main)> house = House.new("$400000", "123 sugar lane")
-  #=> #<House:0x00007fccd30375f8...>
 
 #  pry(main)> house.price
   #=> 400000
