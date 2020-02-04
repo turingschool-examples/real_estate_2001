2cc32c852750e42b9153172292a00e53b14bac20
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-04 10:46:19 -0700

    Create test for rooms by area

--- test/house_test.rb
+++ test/house_test.rb
@@ -87,10 +87,24 @@ class RoomTest < Minitest::Test
     assert_equal 210.53, house.price_per_square_foot
   end
 
+  def test_it_can_sort_rooms_by_area
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    house.rooms_sorted_by_area
+
+    assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
+
+  end
+
 end
-
-#house.rooms_sorted_by_area
-#=> [#<Room:0x00007fccd297dc30...>, #<Room:0x00007fccd383c2d0...>, #<Room:0x00007fccd2985f48...>, #<Room:0x00007fccd29b5720...>]
-
 #house.rooms_by_category
 #=> {:bedroom=>[#<Room:0x00007fccd29b5720...>,
