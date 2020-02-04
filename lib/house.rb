3296005cbc2cb7a8a12523a25fe57e1be636e442
Mariana Cid <cidmariana5@gmail.com>
2020-02-04 09:48:27 -0700

    Methods and test created for rooms from category.

--- lib/house.rb
+++ lib/house.rb
@@ -15,4 +15,16 @@ class House
   def above_market_average?
     @above_market_average
   end
+
+  def rooms_from_category(category)
+    @rooms.map do |room|
+      if room.category == category
+        room
+      end
+    end.compact
+  end
+
+  def area
+    
+  end
 end
