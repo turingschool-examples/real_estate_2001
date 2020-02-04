d1f812877971f3761a1526f367bf3dcb116b58b5
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 09:22:20 -0700

    added attr_reader to the class and changed the price input from string into int

--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,10 @@
 class House
+
+  attr_reader :address, :price
+
   def initialize(price_parameter, address_parameter)
-    @price = price_parameter
+    @price = price_parameter.delete_prefix('$').to_i
     @address = address_parameter
   end
+
 end
