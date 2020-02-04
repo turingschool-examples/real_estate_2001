96a6ef685e9832f29f665bf4f8ee53c5e6ea0922
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 10:01:01 -0700

    added a test to check the price per square foot

--- test/house_test.rb
+++ test/house_test.rb
@@ -66,7 +66,22 @@ class HouseTest < Minitest::Test
 
   def test_house_has_details
     house = House.new("$400000", "123 sugar lane")
-    assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), house.details 
+    assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), house.details
   end
+
+  def test_house_has_price_per_square_foot
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal 210.53, house.price_per_square_foot
+  end
+
+
 
 end
