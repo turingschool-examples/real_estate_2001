775fe24c4f08c3bf8a9d75a579bea7d82af13d51
“Krista <stadler.krista@gmail..com>
2020-02-04 10:46:04 -0700

    Test house details written

--- test/house_test.rb
+++ test/house_test.rb
@@ -71,6 +71,19 @@ class HouseTest < Minitest::Test
     assert_equal 1900, house.area
   end
 
+  def test_house_details
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal
+  end {"price" => 400000, "address" => "123 sugar lane"}, house.details
+
 end
 
 
