4b70645a15aacc20a584373ad95e648f36ad155e
Alex Latham <alexlthm@gmail.com>
2020-02-04 10:41:13 -0700

    Add test for showing rooms by category

--- test/house_test.rb
+++ test/house_test.rb
@@ -114,7 +114,20 @@ class HouseTest < Minitest::Test
 
     assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
   end
-end
 
-# pry(main)> house.rooms_by_category
-# #=> {:bedroom=>[#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>], :living_room=> [#<Room:0x00007fccd383c2d0...>], :basement=> [#<Room:0x00007fccd297dc30...>]}
+  def test_it_can_show_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal ({:bedroom => [room_1, room_2], :living_room => [room_3], :basement => [room_4]}), house.rooms_by_category
+  end
+end
