562ff76cbb6bc69ef7e96088b04855a28320489a
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 09:43:21 -0700

    Add add_room method

--- lib/house.rb
+++ lib/house.rb
@@ -1,12 +1,17 @@
 class House
-  attr_reader :address
+  attr_reader :address, :rooms
 
   def initialize(price_paramater, address_parameter)
     @price = price_paramater
     @address = address_parameter
+    @rooms = []
   end
 
   def price
     @price.gsub("$", '').to_i
+  end
+
+  def add_room(room)
+    @rooms << room
   end
 end
