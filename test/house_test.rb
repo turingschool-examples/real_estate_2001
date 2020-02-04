4edfd09439df3fb50045c53b73d026f71738c90a
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 10:10:38 -0700

    added a test to check if we sort by area

--- test/house_test.rb
+++ test/house_test.rb
@@ -82,6 +82,21 @@ class HouseTest < Minitest::Test
     assert_equal 210.53, house.price_per_square_foot
   end
 
+  def test_house_sorted_by_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [room_4,room_3,room_2,room_1], house.rooms_sorted_by_area
+  end
+
+
+
 
 
 end
