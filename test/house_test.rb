084de13a6dd116c92817ddd8b2e7b381b44ec3a7
Tyler Porter <tylerpporter@gmail.com>
2020-02-04 09:32:29 -0700

    Create add_room method

--- test/house_test.rb
+++ test/house_test.rb
@@ -4,6 +4,8 @@ class HouseTest < Minitest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
   end
 
   def test_it_exists_with_attributes
@@ -12,6 +14,13 @@ class HouseTest < Minitest::Test
     assert_equal 400000, @house.price
     assert_equal [], @house.rooms
     assert_equal "123 sugar lane", @house.address
+  end
+
+  def test_it_can_add_rooms
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+
+    assert_equal [@room_1, @room_2], @house.rooms
   end
 
 end
