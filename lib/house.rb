bd7300d8d2fd71bc6c679d3a1e36004db8a47bfb
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-04 10:13:14 -0700

    Update House: build test/method to return rooms based on their category

--- lib/house.rb
+++ lib/house.rb
@@ -18,4 +18,12 @@ class House
   def above_market_average?
     @price > 500000
   end
+
+  def rooms_from_category(room_category)
+    @rooms.find_all do |room|
+      room.category == room_category
+    end
+  end
+
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -45,5 +45,21 @@ class HouseTest < Minitest::Test
     assert_equal false,  house.above_market_average?
   end
 
+  def test_it_can_look_up_rooms_from_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
 
 end
