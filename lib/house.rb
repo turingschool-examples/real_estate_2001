d53d966eb29dae9f38ce4dc72091fba38a120941
Steven Meyers <sjjmeyers@gmail.com>
2020-02-04 10:38:52 -0700

    Add room method and test

--- lib/house.rb
+++ lib/house.rb
@@ -1,15 +1,19 @@
 class House
+  attr_reader :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
   end
 
   def price
     @price.to_i
   end
 
-
+def add_room(room)
+  @rooms << room
+end
 
 
 
--- test/house_test.rb
+++ test/house_test.rb
@@ -13,7 +13,20 @@ class HouseTest < Minitest::Test
 
   def test_it_has_price_and_address
     house = House.new("400000", "123 sugar lane")
-    
+
     assert_equal 400000, house.price
+  end
+
+  def test_if_it_has_and_can_add_rooms
+    house = House.new("400000", "123 sugar lane")
+
+    assert_equal [], house.rooms
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
   end
 end
