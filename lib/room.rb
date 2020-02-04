18e885a7484e194bf3184ea164510c39f83b42c9
Colin Alexander <colintalex@gmail.com>
2020-02-04 09:43:36 -0700

    Adds instance varibale @coats_of_paint, painted? method and paint method. Tested and functioning.

--- lib/room.rb
+++ lib/room.rb
@@ -4,9 +4,22 @@ class Room
     @category = category
     @length = length
     @width = width
+    @coats_of_paint = 0
   end
 
   def area
     @length.to_f * @width.to_f
+  end
+
+  def painted?
+    if @coats_of_paint == 0
+      false
+    else
+      true
+    end
+  end
+
+  def paint
+    @coats_of_paint +=1
   end
 end
