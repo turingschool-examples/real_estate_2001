e5183158b78c83df9502650957081f181158a490
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-04 09:55:22 -0700

    Add add_room method. Edit tests to remove setup method

--- lib/house.rb
+++ lib/house.rb
@@ -1,44 +1,14 @@
-require './lib/room'
-
 class House
 
+  attr_reader :price, :address, :rooms
+
   def initialize(price, address)
-    @price = price
+    @price = price[1..-1].to_i
     @address = address
     @rooms = []
   end
 
+  def add_room(room)
+    @rooms << room
+  end
 end
-
-
-
-# pry(main)> require './lib/room'
-# #=> true
-#
-# pry(main)> require './lib/house'
-# #=> true
-#
-# pry(main)> house = House.new("$400000", "123 sugar lane")
-# #=> #<House:0x00007fccd30375f8...>
-#
-# pry(main)> house.price
-# #=> 400000
-#
-# pry(main)> house.address
-# #=> "123 sugar lane"
-#
-# pry(main)> house.rooms
-# #=> []
-#
-# pry(main)> room_1 = Room.new(:bedroom, 10, '13')
-# #=> #<Room:0x00007fccd29b5720...>
-#
-# pry(main)> room_2 = Room.new(:bedroom, 11, '15')
-# #=> #<Room:0x00007fccd2985f48...>
-#
-# pry(main)> house.add_room(room_1)
-#
-# pry(main)> house.add_room(room_2)
-#
-# pry(main)> house.rooms
-# #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
--- test/house_test.rb
+++ test/house_test.rb
@@ -5,11 +5,9 @@ require './lib/house'
 
 class HouseTest < Minitest::Test
 
-  def setup
-    house = House.new("$400000", "123 sugar lane")
-  end
-
   def test_it_exists_and_has_attributes
+    house = House.new("$400000", "123 sugar lane")
+
     assert_instance_of House, house
     assert_equal 400000, house.price
     assert_equal "123 sugar lane", house.address
@@ -17,6 +15,7 @@ class HouseTest < Minitest::Test
   end
 
   def test_it_can_have_rooms
+    house = House.new("$400000", "123 sugar lane")
     room_1 = Room.new(:bedroom, 10, '13')
 
     assert_instance_of Room, room_1
@@ -27,6 +26,9 @@ class HouseTest < Minitest::Test
   end
 
   def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
     house.add_room(room_1)
     house.add_room(room_2)
 
