25764f88d723c111bda44644d425e6807437c9d6
Andrew Reid <areid.sj@gmail.com>
2020-02-04 09:24:22 -0700

    Write rooms can be added test

--- test/house_test.rb
+++ test/house_test.rb
@@ -7,6 +7,8 @@ class RoomTest < Minitest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
     # require "pry"; binding.pry
   end
 
@@ -24,6 +26,12 @@ class RoomTest < Minitest::Test
 
   def test_it_starts_with_no_rooms
     assert_equal [], @house.rooms
+  end
+
+  def test_rooms_can_be_added
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    assert_equal [@room_1, @room_2], house.rooms
   end
 
 end
