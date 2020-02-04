4e22b035c7f40e06c9f8e34fdc92d7e532e1f5bb
Alex Latham <alexlthm@gmail.com>
2020-02-04 09:47:44 -0700

    Add test for listing rooms by category

--- test/house_test.rb
+++ test/house_test.rb
@@ -43,32 +43,49 @@ class HouseTest < Minitest::Test
     house2 = House.new("$600000", "123 sugar lane")
     assert_equal true, house2.above_market_average?
   end
+
+  def test_it_can_list_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
 end
 
-# pry(main)> room_1 = Room.new(:bedroom, 10, '13')
+# pry(main)>
 # #=> #<Room:0x00007fccd29b5720...>
 #
-# pry(main)> room_2 = Room.new(:bedroom, 11, '15')
+# pry(main)>
 # #=> #<Room:0x00007fccd2985f48...>
 #
-# pry(main)> room_3 = Room.new(:living_room, 25, '15')
+# pry(main)>
 # #=> #<Room:0x00007fccd383c2d0...>
 #
-# pry(main)> room_4 = Room.new(:basement, 30, '41')
+# pry(main)>
 # #=> #<Room:0x00007fccd297dc30...>
 #
-# pry(main)> house.add_room(room_1)
+# pry(main)>
 #
-# pry(main)> house.add_room(room_2)
+# pry(main)>
 #
-# pry(main)> house.add_room(room_3)
+# pry(main)>
 #
-# pry(main)> house.add_room(room_4)
+# pry(main)>
 #
-# pry(main)> house.rooms_from_category(:bedroom)
+# pry(main)>
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
 #
-# pry(main)> house.rooms_from_category(:basement)
+# pry(main)>
 # #=> [#<Room:0x00007fccd297dc30...>]
 #
 # pry(main)> house.area
