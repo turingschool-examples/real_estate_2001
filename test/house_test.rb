fb066bc277fd2f929873ed04655be61a69a780de
Alex Latham <alexlthm@gmail.com>
2020-02-04 10:32:19 -0700

    Fix typo in assertion for room sorting test

--- test/house_test.rb
+++ test/house_test.rb
@@ -112,7 +112,7 @@ class HouseTest < Minitest::Test
     house.add_room(room_3)
     house.add_room(room_4)
 
-    assert_equal [room_4, room_3, room_2, room_1]. house.rooms_sorted_by_area
+    assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
   end
 end
 
