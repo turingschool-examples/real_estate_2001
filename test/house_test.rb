2150a3970276944e0a6002b3baa00c66d6a72710
Andrew Reid <areid.sj@gmail.com>
2020-02-04 09:45:52 -0700

    Write price per sq ft test

--- test/house_test.rb
+++ test/house_test.rb
@@ -62,4 +62,13 @@ class RoomTest < Minitest::Test
     assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), @house.details
   end
 
+  def test_it_has_price_per_square_foot
+    @house.add_room(@room_4)
+    @house.add_room(@room_1)
+    @house.add_room(@room_3)
+    @house.add_room(@room_2)
+
+    assert_equal 210.53, @house.price_per_square_foot
+  end
+
 end
