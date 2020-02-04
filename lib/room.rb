24237f31a9356c3c28c8be063e4c12718ed63311
Chris Stankus <chrisstankus99@gmail.com>
2020-02-04 09:14:55 -0700

    Add is painted and paint methods and tests, make current tests pass

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,22 @@
-class Rom
+class Room
 
+  attr_reader :category
   def initialize(category, length, width)
     @category = category
-    @length = 10
-    @width = width
+    @length = length
+    @width = width.to_i
+    @painted = false
   end
 
   def area
     @length * @width
+  end
+
+  def is_painted?
+    @painted
+  end
+
+  def paint
+    @painted = true
   end
 end
