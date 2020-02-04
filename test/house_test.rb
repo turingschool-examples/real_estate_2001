621e667f5736db1de45d1074650789819e3eeeaa
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-04 10:31:24 -0700

    add rooms by category test

--- test/house_test.rb
+++ test/house_test.rb
@@ -40,4 +40,18 @@ class HouseTest < Minitest::Test
     assert_equal house.above_market_average?, false
   end
 
+  def test_it_sorts_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal house.rooms_from_category(:bedroom), [room_1, room_2]
+    assert_equal house.rooms_from_category(:basement), [room_4]
+  end
+
 end
