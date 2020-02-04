c68812602cb807723001919612629457c3d5ce6c
Ethan Hocking <eshocking253@gmail.com>
2020-02-04 10:18:37 -0700

    Add above_market_average? method

--- lib/house.rb
+++ lib/house.rb
@@ -9,4 +9,8 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    @price > 500000
+  end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -29,17 +29,49 @@ class HouseTest < Minitest::Test
 
     assert_equal [room_1, room_2], house.rooms
   end
+
+  def test_returns_above_market_average
+    house1 = House.new("$400000", "123 sugar lane")
+    house2 = House.new("$600000", "123 sesame street")
+
+
+    assert_equal false, house1.above_market_average?
+    assert_equal true, house2.above_market_average?
+  end
 end
 
+
+# pry(main)> house.above_market_average?
+# #=> false
+#
 # pry(main)> room_1 = Room.new(:bedroom, 10, '13')
 # #=> #<Room:0x00007fccd29b5720...>
 #
 # pry(main)> room_2 = Room.new(:bedroom, 11, '15')
 # #=> #<Room:0x00007fccd2985f48...>
 #
+# pry(main)> room_3 = Room.new(:living_room, 25, '15')
+# #=> #<Room:0x00007fccd383c2d0...>
+#
+# pry(main)> room_4 = Room.new(:basement, 30, '41')
+# #=> #<Room:0x00007fccd297dc30...>
+#
 # pry(main)> house.add_room(room_1)
 #
 # pry(main)> house.add_room(room_2)
 #
-# pry(main)> house.rooms
+# pry(main)> house.add_room(room_3)
+#
+# pry(main)> house.add_room(room_4)
+#
+# pry(main)> house.rooms_from_category(:bedroom)
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
+#
+# pry(main)> house.rooms_from_category(:basement)
+# #=> [#<Room:0x00007fccd297dc30...>]
+#
+# pry(main)> house.area
+# #=> 1900
+#
+# pry(main)> house.details
+# #=> {"price" => 400000, "address" => "123 sugar lane"}
