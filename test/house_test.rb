d721a8c45ad4be67d7366bedda6e6abb42480986
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 09:55:47 -0700

    added a test to check details

--- test/house_test.rb
+++ test/house_test.rb
@@ -64,5 +64,9 @@ class HouseTest < Minitest::Test
     assert_equal 1900, house.area
   end
 
+  def test_house_has_details
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), house.details 
+  end
 
 end
