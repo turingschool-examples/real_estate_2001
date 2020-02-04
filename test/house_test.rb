5e10b4722d01c670eb7df6e76604399e25cbf4e4
“Krista <stadler.krista@gmail..com>
2020-02-04 09:47:01 -0700

    Test rooms added written

--- test/house_test.rb
+++ test/house_test.rb
@@ -24,27 +24,21 @@ class HouseTest < Minitest::Test
   end
 
   def test_it_has_rooms
-
+    skip
     house = House.new("$400000", "123 sugar lane")
     assert_equal [], house.rooms
   end
 
-
+  def test_rooms_added
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    assert_equal (room1, room2), house.rooms
+  end
 end
 
-
-#  pry(main)> house.rooms
-  #=> []
-
-#  pry(main)> room_1 = Room.new(:bedroom, 10, '13')
-  #=> #<Room:0x00007fccd29b5720...>
-
-#  pry(main)> room_2 = Room.new(:bedroom, 11, '15')
-  #=> #<Room:0x00007fccd2985f48...>
-
-#  pry(main)> house.add_room(room_1)
-
-#  pry(main)> house.add_room(room_2)
 
 #  pry(main)> house.rooms
   #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
