1e2276ff0dfb6cde56e84d4e8bfa901662d8bb5d
Ryan Camp <cmpprg@icloud.com>
2020-02-04 09:11:39 -0700

    Create is_painted? method with passing test

--- lib/room.rb
+++ lib/room.rb
@@ -9,4 +9,8 @@ class Room
   def area
     @length * @width
   end
+
+  def is_painted?
+    false
+  end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -15,10 +15,16 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_can_get_area
-    
+
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
     assert_equal 180, room2.area
+  end
+
+  def test_is_painted_method
+    room1 = Room.new(:bedroom, 10, '13')
+
+    assert_equal false, room1.is_painted?
   end
 end
