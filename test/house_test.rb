4212c8f76daa7d0129b61eb6bff23288985ffbb9
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-04 10:49:49 -0700

    Add test_it_can_return_house_area and test_house_has_details

--- test/house_test.rb
+++ test/house_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/room'
-require './lib/house'
+require "./lib/room"
+require "./lib/house"
 
 class HouseTest < Minitest::Test
   def test_it_exists
@@ -37,31 +37,47 @@ class HouseTest < Minitest::Test
 
   end
 
-  def test_it_returns_above_market_average_true
-    skip
-      house = House.new("$600000", "123 sugar lane")
-      house.above_market_average?
+  def test_it_can_list_rooms_by_category
+   house = House.new("$400000", "123 sugar lane")
 
-      assert_equal true, house.above_market_average?
+   room_1 = Room.new(:bedroom, 10, '13')
+   room_2 = Room.new(:bedroom, 11, '15')
+   room_3 = Room.new(:living_room, 25, '15')
+   room_4 = Room.new(:basement, 30, '41')
 
+   house.add_room(room_1)
+   house.add_room(room_2)
+   house.add_room(room_3)
+   house.add_room(room_4)
+
+   assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+   assert_equal [room_4], house.rooms_from_category(:basement)
+ end
+
+  def test_it_can_return_house_area
+     house = House.new("$400000", "123 sugar lane")
+
+     room_1 = Room.new(:bedroom, 10, '13')
+     room_2 = Room.new(:bedroom, 11, '15')
+     room_3 = Room.new(:living_room, 25, '15')
+     room_4 = Room.new(:basement, 30, '41')
+
+     house.add_room(room_1)
+     house.add_room(room_2)
+     house.add_room(room_3)
+     house.add_room(room_4)
+
+     assert_equal 1900, house.area
+   end
+
+
+  def test_house_has_details
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), house.details
   end
 
-  def test_returns_rooms_from_category
-      house = House.new("$400000", "123 sugar lane")
-      room_1 = Room.new(:bedroom, 10, '13')
-      room_2 = Room.new(:bedroom, 11, '15')
-      room_3 = Room.new(:living_room, 25, '15')
-      room_4 = Room.new(:basement, 30, '41')
 
-      house.add_room(room_1)
-      house.add_room(room_2)
-      house.add_room(room_3)
-      house.add_room(room_4)
-      house.rooms_from_category(:bedroom)
-
-      assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
-
-  end
 
 
 
