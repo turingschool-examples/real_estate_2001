f2c55416ccdc47dde6cf59744c24253e026d1c8c
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-04 09:11:45 -0700

    Complete room class and test

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,22 @@
-class Rom
+class Room
+  attr_reader :category, :length, :width
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
+    @length = length
     @width = width
+    @paint = false
   end
 
   def area
-    @length * @width
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    @paint
+  end
+
+  def paint
+    @paint = true
   end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -9,16 +9,32 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
     assert_equal 180, room2.area
   end
+
+  def test_room_is_not_painted_by_default
+    room1 = Room.new(:bedroom, 10, '13')
+    room2 = Room.new(:living_room, 15, '12')
+    assert_equal false, room1.is_painted?
+    room2.paint
+    assert_equal true, room2.is_painted?
+  end
 end
+
+
+
+# pry(main)> room.is_painted?
+# #=> false
+#
+# pry(main)> room.paint
+#
+# pry(main)> room.is_painted?
+# #=> true
