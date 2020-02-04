69b13e9c74a4f3e0192ca3e91138b4ecc022948e
Ethan Hocking <eshocking253@gmail.com>
2020-02-04 10:00:22 -0700

    Add rooms attribute, add conversion for price attribute, add attr_readers for price, address, and rooms

--- test/house_test.rb
+++ test/house_test.rb
@@ -8,20 +8,18 @@ class HouseTest < Minitest::Test
   def test_it_exists
     house = House.new("$400000", "123 sugar lane")
 
-    assert_instance_of House, house 
+    assert_instance_of House, house
   end
 
+  def test_has_attributes
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal 400000, house.price
+    assert_equal "123 sugar lane", house.address
+    assert_equal [], house.rooms 
+  end
 end
 
-# pry(main)> require './lib/room'
-# #=> true
-#
-# pry(main)> require './lib/house'
-# #=> true
-#
-# pry(main)> house = House.new("$400000", "123 sugar lane")
-# #=> #<House:0x00007fccd30375f8...>
-#
 # pry(main)> house.price
 # #=> 400000
 #
