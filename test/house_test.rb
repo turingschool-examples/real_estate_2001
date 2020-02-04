f01b1963ee7a26a4cb6c4c44b1fd65396290c4c4
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-04 09:42:18 -0700

    Add test_it_can_add_rooms

--- test/house_test.rb
+++ test/house_test.rb
@@ -13,8 +13,22 @@ class HouseTest < Minitest::Test
   def test_rooms_starts_as_empty_array
     house = House.new("$400000", "123 sugar lane")
     house.rooms
-    
+
     assert_equal [], house.rooms
+  end
+
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.rooms
+
+    assert_equal [room_1, room_2], house.rooms   
+
+
   end
 
 
