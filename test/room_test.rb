48d8aaa7e54ae789fb0e171f4d4d4fbc63dd16cf
Tyler Porter <tylerpporter@gmail.com>
2020-02-04 09:06:55 -0700

    Refactor tests

--- test/room_test.rb
+++ test/room_test.rb
@@ -3,22 +3,28 @@ require 'minitest/pride'
 require './lib/room'
 
 class RoomTest < Minitest::Test
+  def setup
+    @room1 = Room.new(:bedroom, 10, '13')
+    @room2 = Room.new(:living_room, 15, '12')
+  end
+
   def test_it_exists
-    room = Room.new(:bedroom, 10, '13')
-    assert_instance_of Room, room
+
+    assert_instance_of Room, @room1
   end
 
   def test_it_has_a_category
 
-    room = Room.new(:bedroom, 10, '13')
-    assert_equal :bedroom, room.category
+    assert_equal :bedroom, @room1.category
   end
 
   def test_it_can_get_area
-    
-    room1 = Room.new(:bedroom, 10, '13')
-    room2 = Room.new(:living_room, 15, '12')
-    assert_equal 130, room1.area
-    assert_equal 180, room2.area
+
+    assert_equal 130, @room1.area
+    assert_equal 180, @room2.area
+  end
+
+  def test_it_is_not_painted_by_default
+    skip
   end
 end
