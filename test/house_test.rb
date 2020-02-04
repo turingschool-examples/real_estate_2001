6402dfbd07c7b2d949f1e78f2b80d81fd2a4e3a1
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 09:19:05 -0700

    added test to check the price and address

--- test/house_test.rb
+++ test/house_test.rb
@@ -9,4 +9,14 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     assert_instance_of House, house
   end
+
+  def test_if_house_has_price
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal 400000, house.price
+  end
+
+  def test_if_house_has_address
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "123 sugar lane", house.address
+  end
 end
