14530631718da30b2940366584ba2007aa3769a8
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-04 10:12:39 -0700

    Iteration 3, all but hash testing details

--- lib/house.rb
+++ lib/house.rb
@@ -11,4 +11,34 @@ attr_reader :price, :address, :rooms
     rooms << room
   end
 
+  def above_market_average?
+    if @price[1..-1].to_i > 500000
+      true
+    else
+      false
+    end
+  end
+
+  def rooms_from_category(category)
+    rooms_by_category = []
+    @rooms.each do |room|
+      if room.category == category
+        rooms_by_category << room
+      end
+    end
+    rooms_by_category
+  end
+
+  def area
+    all_room_areas = []
+    @rooms.each do |room|
+      all_room_areas << room.area
+    end
+    all_room_areas.sum
+  end
+
+  def details
+    details = {price: @price[1..-1].to_i, address: @address}
+  end
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -22,4 +22,42 @@ class HouseTest < Minitest::Test
     assert_equal [room_1, room_2], house.rooms
   end
 
+  def test_above_market_average
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal false, house.above_market_average?
+  end
+
+  def test_rooms_from_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_house_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal 1900, house.area
+  end
+
+
+  def test_house_details
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal [:price => 400000, :address => "123 sugar lane"], house.details
+  end
+
 end
