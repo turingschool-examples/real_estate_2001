2e3c570ef162d45ceb77c7605052acf2de399bf9
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-04 10:06:16 -0700

    Add rooms_from_category method

--- lib/house.rb
+++ lib/house.rb
@@ -1,22 +1,30 @@
 class House
-attr_reader :price, :address, :rooms
-  def initialize(price, address)
-    @price = price
-    @address = address
-    @rooms = []
-  end
+  attr_reader :price, :address, :rooms
+    def initialize(price, address)
+      @price = price
+      @address = address
+      @rooms = []
+    end
 
-  def add_room(new_room)
-    @rooms << new_room
-  end
+    def add_room(new_room)
+      @rooms << new_room
+    end
 
-  def above_market_average?
-    if @price.to_i > 500000
-      true
-    else
-      false
+    def above_market_average?
+      if @price.to_i > 500000
+        true
+      else
+        @price.to_i < 500000
+        false
+      end
+    end
+
+    def rooms_from_category(category)
+      rooms_from_category = []
+      @rooms.select do |room|
+        if room.category == category
+          rooms_from_category << room
+        end
     end
   end
-
-
 end
