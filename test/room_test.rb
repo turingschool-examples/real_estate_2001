39311da874ff63df6faa8d739a2dbb8c106461ec
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-04 09:07:35 -0700

    Add category method, improve dynamic functionality

--- test/room_test.rb
+++ test/room_test.rb
@@ -9,13 +9,13 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
+    # skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
