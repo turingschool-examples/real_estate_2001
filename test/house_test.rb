47c2461427f5089a7d4eb257a3bab2f9f2d02e20
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 09:39:20 -0700

    created a test to check rooms from certain categories

--- test/house_test.rb
+++ test/house_test.rb
@@ -36,4 +36,17 @@ class HouseTest < Minitest::Test
     house1 = House.new("$600000", "111 fake lane")
     assert_equal true, house1.above_market_average?
   end
+
+  def test_house_has_rooms_from_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+  end
 end
