ed9c5e1f717260923c385a9b2094d7a4712984a5
Ryan Camp <cmpprg@icloud.com>
2020-02-04 09:40:30 -0700

    Create add_room method with test passed

--- test/house_test.rb
+++ test/house_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/house.rb"
+require "./lib/room.rb"
 
 class HouseTest < Minitest::Test
   def test_it_exists
@@ -15,5 +16,18 @@ class HouseTest < Minitest::Test
     assert_equal 400000, house.price
     assert_equal "123 sugar lane", house.address
     assert_equal [], house.rooms
+  end
+
+  def test_add_room_method
+    house = House.new("$400000", "123 sugar lane")
+    room1 = Room.new(:bedroom, 10, '13')
+    room2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room1)
+
+    assert_equal [room1], house.rooms
+
+    house.add_room(room2)
+
+    assert_equal [room1, room2], house.rooms
   end
 end
