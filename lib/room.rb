8fb30d13549f41ea320670ec50ab4136f2cfa36f
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-09 16:43:28 -0600

    Redo Iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,15 +1,22 @@
 class Room
-  attr_reader :category, :length, :width
+  attr_reader :category
 
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
+    @painted = false
   end
 
   def area
-    length * width
+    @length * @width.to_i
   end
 
+  def is_painted?
+    @painted
+  end
 
+  def paint
+    @painted = true
+  end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -6,20 +6,27 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
   def test_it_exists
-    room = Room.new(:bedroom, 10, 13)
+    room = Room.new(:bedroom, 10, '13')
     assert_instance_of Room, room
   end
 
   def test_it_has_a_category
-    room = Room.new(:bedroom, 10, 13)
+    room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
+  def test_it_is_painted
+    room = Room.new(:bedroom, 10, '13')
+    assert_equal false, room.is_painted?
+    room.paint
+    assert_equal true, room.is_painted?
+  end
+
   def test_it_can_get_area
-    room1 = Room.new(:bedroom, 10, 13)
-    room2 = Room.new(:living_room, 15, 12)
-    assert_equal 130, room1.area
-    assert_equal 180, room2.area
+    room = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    assert_equal 130, room.area
+    assert_equal 165, room_2.area
   end
 
 
